0a9621296c3465f616ace13eadb38db331263272
ferdinandimeka <117474473+ferdinandimeka@users.noreply.github.com>
2023-05-08 03:29:06 +0100

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,6 +14,6 @@ echo "<html>
 </html>" > /data/web_static/releases/test/index.html
 ln -fs /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data
-sed -i '/listen 80 default_server/ a\\tlocation /hbnb_static/{\n alias /data/web_static/current;\n\t\}\n' /etc/nginx/sites-available/default
+sed -i '/listen 80 default_server/ a\\tlocation /hbnb_static {\n alias /data/web_static/current;\n\t\}\n' /etc/nginx/sites-available/default
 service nginx restart
 exit (0)
