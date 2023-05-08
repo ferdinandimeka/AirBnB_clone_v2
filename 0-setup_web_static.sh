9c91170678e27e9d503aabca42a26470adb4724c
ferdinandimeka <117474473+ferdinandimeka@users.noreply.github.com>
2023-05-08 03:36:42 +0100

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,12 @@
 #!/usr/bin/env bash
-# setup server for deployment web_static
-apt-get upgrade -y
-apt-get update
-apt-get -y install nginx
-ufw allow 'Nginx HTTP'
-mkdir -p /data/web_static/releases/test /data/web_static/shared /data/web_static/current
-echo "<html>
-    <head>
-    </head>
-    <body>
-        Holberton School
-    </body>
-</html>" > /data/web_static/releases/test/index.html
-ln -fs /data/web_static/releases/test /data/web_static/current
-chown -R ubuntu:ubuntu /data
-sed -i '/listen 80 default_server/ a\\tlocation /hbnb_static {\n alias /data/web_static/current;\n\t\}\n' /etc/nginx/sites-available/default
-service nginx restart
+# sets up the web servers for the deployment of web_static
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '38i\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx start
