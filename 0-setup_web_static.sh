32cfb9899b76a3a6336b6079728d8a71df5c83f3
ferdinandimeka <117474473+ferdinandimeka@users.noreply.github.com>
2023-05-08 03:33:38 +0100

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,19 +1,29 @@
 #!/usr/bin/env bash
-# setup server for deployment web_static
-apt-get upgrade -y
-apt-get update
-apt-get -y install nginx
-ufw allow 'Nginx HTTP'
-mkdir -p /data/web_static/{releases/test, shared}
-echo "<html>
-    <head>
-    </head>
-    <body>
-        Holberton School
-    </body>
-</html>" > /data/web_static/releases/test/index.html
-ln -fs /data/web_static/releases/test /data/web_static/current
-chown -R ubuntu:ubuntu /data
-sed -i '/listen 80 default_server/ a\\tlocation /hbnb_static {\n alias /data/web_static/current;\n\t\}\n' /etc/nginx/sites-available/default
-service nginx restart
-exit (0)
+
+# Install Nginx if it's not already installed
+if ! command -v nginx > /dev/null; then
+    sudo apt-get update
+    sudo apt-get -y install nginx
+fi
+
+# Create necessary directories
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared /data/web_static/current
+
+# Create a fake HTML file for testing
+echo "Test page" | sudo tee /data/web_static/releases/test/index.html
+
+# Create a symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Give ownership of /data/ folder to the ubuntu user and group
+sudo chown -R ubuntu:ubuntu /data/
+
+# Update Nginx configuration
+CONFIG_FILE=/etc/nginx/sites-available/default
+sudo sed -i "/^\s*location \/ {$/,/^\s*}$/s/^/\t/" $CONFIG_FILE
+sudo sed -i "/^\s*location \/ {$/,/^\s*}$/a \\\talias /data/web_static/current/;" $CONFIG_FILE
+
+# Restart Nginx
+sudo service nginx restart
+
+exit 0
